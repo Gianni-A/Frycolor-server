6482cdbe1c5019ae6022ec3e5bcbf8461c444417
Gianni Alvarado <giannialvaradoc@gmail.com>
2020-05-31 20:16:42 -0500

    Add query to count news reactions by nw_id

--- src/main/java/com/gianni/frycolor/repository/NewsReactionDao.java
+++ src/main/java/com/gianni/frycolor/repository/NewsReactionDao.java
@@ -1,6 +1,8 @@
 package com.gianni.frycolor.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.gianni.frycolor.entities.NewsReaction;
@@ -8,5 +10,6 @@ import com.gianni.frycolor.entities.NewsReaction;
 @Repository
 public interface NewsReactionDao extends JpaRepository<NewsReaction, Integer> {
 
-	
+	@Query(value = "SELECT count(1) FROM news_reaction WHERE nw_id = :nwId", nativeQuery=true)
+	int getCountReactionNews(@Param("nwId") int nwId);
 }
