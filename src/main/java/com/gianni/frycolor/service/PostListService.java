1cb0487c7633fa38b21cb64d5ee193f0ec93331c
Gianni Alvarado <giannialvaradoc@gmail.com>
2020-09-16 23:43:11 -0500

    Listed photos by userId

--- src/main/java/com/gianni/frycolor/service/PostListService.java
+++ src/main/java/com/gianni/frycolor/service/PostListService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.gianni.frycolor.exception.PostListException;
 import com.gianni.frycolor.model.Post;
 import com.gianni.frycolor.model.PostModel;
 import com.gianni.frycolor.model.ResponsePost;
@@ -48,12 +49,48 @@ public class PostListService {
 			
 		});
 		
+		if(listPost.size() == 0) {
+			throw new PostListException("There is no post published");
+		}
+		
 		return listPost;
 	}
 	
-	//Needs to test later or when it is the moment
+	//It is the same as above method but adding a filter to get just post with images
 	public List<Post> getNewsJustImages(int userId, int pagination) {
-		return null;
+		List<Post> listPhotos = new ArrayList<>();
+		List<PostModel> list = repositoryImpl.getAllListPost(userId);
+		list.stream()
+			.filter(p -> p.getPathImage() != null)
+			.forEach(p -> {
+				Post post = new Post();
+				
+				post.setNwId(p.getNwId());
+				post.setImageProfile(p.getImageProfile());
+				//Be careful on the name, if the user doesn't have a lastname, the query will get you the name null complete
+				post.setNameUser(p.getNameUser());
+				
+				if(p.getComment() != null) post.setComment(p.getComment());
+				
+				if(p.getPathImage() != null) post.setImage(p.getPathImage());
+				
+				int contReactions = repositoryImpl.getTotalReactionsByNwId(p.getNwId());
+				
+				post.setContReactions(contReactions);
+				post.setUserLike( p.getUserLike() != null ? true : false );
+				
+				List<ResponsePost> listResponses = repoResponseImpl.getAllResponseFromPost(p.getNwId());
+				post.setListResponses(listResponses);
+				
+				listPhotos.add(post);
+				
+			});
+		
+		if(listPhotos.size() == 0) {
+			throw new PostListException("There is no photos published");
+		}
+		
+		return listPhotos;
 	}
 
 }
