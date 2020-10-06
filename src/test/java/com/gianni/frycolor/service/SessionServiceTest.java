78629c02a5e0cd1ec9aec49a4a76f27a423d5f15
Gianni Alvarado <giannialvaradoc@gmail.com>
2020-10-05 22:31:46 -0500

    Add unitTest for SessionService

--- src/test/java/com/gianni/frycolor/service/SessionServiceTest.java
+++ src/test/java/com/gianni/frycolor/service/SessionServiceTest.java
@@ -22,20 +22,45 @@ import org.powermock.modules.junit4.PowerMockRunner;
 import com.gianni.frycolor.entities.User;
 import com.gianni.frycolor.exception.EmailException;
 import com.gianni.frycolor.exception.UserExistException;
+import com.gianni.frycolor.exception.UserValidationsException;
 import com.gianni.frycolor.information.UserInfo;
 import com.gianni.frycolor.repository.SessionDao;
 import com.gianni.frycolor.util.SendMail;
+
+import org.junit.Assert;
 
 @PrepareForTest({SendMail.class})
 @RunWith(PowerMockRunner.class)
 public class SessionServiceTest {
 	
 	private SessionService service;
+	private SessionDao mockSession;
+	
+	private UserInfo userInfo;
+	
+	private final String USERNAME = "userTest";
+	private final String PASSWORD = "passwordTest";
 	
 	@Before
 	public void setup() {
 		service = new SessionService();
 		service.repository = mock(SessionDao.class);
+		mockSession = service.repository;
+		userInfo = new UserInfo();
+	}
+	
+	@Test(expected = UserValidationsException.class)
+	public void logInException() {
+		when(mockSession.logIn(anyString(), anyString())).thenReturn(null);
+		service.logIn(USERNAME, PASSWORD);
+	}
+	
+	@Test
+	public void logInTest() {
+		when(mockSession.logIn(anyString(), anyString())).thenReturn(userInfo.getUser());
+		User user = service.logIn(USERNAME, PASSWORD);
+		Assert.assertEquals("Test", user.getUsUser());
+		Assert.assertEquals("123456", user.getUsPassword());
 	}
 	
 	@Test(expected = EmailException.class)
@@ -47,14 +72,14 @@ public class SessionServiceTest {
 	@Test(expected = UserExistException.class)
 	public void sendChangePasswordUserNotFoundTest() {
 		String email = "test@gmail.com";
-		when(service.repository.getUserIdByEmail(anyString())).thenReturn(null);
+		when(mockSession.getUserIdByEmail(anyString())).thenReturn(null);
 		service.sendChangesPassword(email);
 	}
 	
 	@Test
 	public void sendChangePasswordTest() throws AddressException, MessagingException, IOException {
 		String email = "test@gmail.com";
-		when(service.repository.getUserIdByEmail(anyString())).thenReturn(10);
+		when(mockSession.getUserIdByEmail(anyString())).thenReturn(10);
 		
 		PowerMockito.mockStatic(SendMail.class);
 		
@@ -65,17 +90,25 @@ public class SessionServiceTest {
 		
 	}
 	
+	@Test(expected = UserExistException.class)
+	public void changePasswordForgottenException() {
+		String userId = "MQ==";
+		String password = "987654";
+		when(mockSession.getUserCredentials(anyInt())).thenReturn(null);
+		service.changePasswordForgotten(userId, password);
+	}
+	
 	@Test
 	public void changePasswordForgottenTest() {
 		String userId = "MQ==";
 		String password = "987654";
 		UserInfo userInfo = new UserInfo();
 		
-		when(service.repository.getUserCredentials(anyInt())).thenReturn(userInfo.getUser());
+		when(mockSession.getUserCredentials(anyInt())).thenReturn(userInfo.getUser());
 		
 		service.changePasswordForgotten(userId, password);
 		
-		verify(service.repository).save(any(User.class));
+		verify(mockSession).save(any(User.class));
 	}
 
 }
